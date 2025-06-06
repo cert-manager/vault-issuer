cert-manager/cert-manager

Refactor Issuer interface, making an issuer work for multiple resource instances. Issuers are now provided via the Setup function.
Signed-off-by: Tim Ramlot <42113979+inteon@users.noreply.github.com>

pkg/issuer/vault/setup_test.go
@@ -433,9 +433,7 @@ func TestVault_Setup(t *testing.T) {
 			cmclient := cmfake.NewSimpleClientset(givenIssuer)
 
 			v := &Vault{
-				issuer:            givenIssuer,
-				Context:           &controller.Context{CMClient: cmclient},
-				resourceNamespace: "test-namespace",
+				Context: &controller.Context{CMClient: cmclient},
 				createTokenFn: func(ns string) vaultinternal.CreateToken {
 					return func(ctx context.Context, saName string, req *authv1.TokenRequest, opts metav1.CreateOptions) (*authv1.TokenRequest, error) {
 						return &authv1.TokenRequest{Status: authv1.TokenRequestStatus{
@@ -459,7 +457,7 @@ func TestVault_Setup(t *testing.T) {
 				},
 			}
 
-			err := v.Setup(context.Background())
+			err := v.Setup(context.Background(), givenIssuer)
 			if tt.expectErr != "" {
 				assert.EqualError(t, err, tt.expectErr)
 				return

cert-manager/vault-issuer

pkg/issuer/vault/setup_test.go
@@ -433,9 +433,7 @@ func TestVault_Setup(t *testing.T) {
 			cmclient := cmfake.NewSimpleClientset(givenIssuer)
 
 			v := &Vault{
-				issuer:            givenIssuer,
-				Context:           &controller.Context{CMClient: cmclient},
-				resourceNamespace: "test-namespace",
+				Context: &controller.Context{CMClient: cmclient},
 				createTokenFn: func(ns string) vaultinternal.CreateToken {
 					return func(ctx context.Context, saName string, req *authv1.TokenRequest, opts metav1.CreateOptions) (*authv1.TokenRequest, error) {
 						return &authv1.TokenRequest{Status: authv1.TokenRequestStatus{
@@ -459,7 +457,7 @@ func TestVault_Setup(t *testing.T) {
 				},
 			}
 
-			err := v.Setup(t.Context())
+			err := v.Setup(t.Context(), givenIssuer)
 			if tt.expectErr != "" {
 				assert.EqualError(t, err, tt.expectErr)
 				return